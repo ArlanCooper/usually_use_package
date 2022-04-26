6ac82b46c67838d430dc0e055b50af83da3b74d4
cooper <45280520+ArlanCooper@users.noreply.github.com>
2022-04-26 16:37:09 +0800

    初始化，编辑距离函数

---
+++ data_process/text_operate/text_operate.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+
+
+class TextOperate():
+    
+    def edit_distance(self,s1,s2):
+        '''
+        求取s1与s2的编辑距离
+        
+        '''
+        m,n = len(s1),len(s2)
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif s1[i-1] ==s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1]) + 1
+        return dp[-1][-1]
+    def edit_distance_v1(self,s1,s2):
+        '''
+        求取s1与s2的编辑距离，优化内存版
+        '''
+        m=len(s1)
+        n=len(s2)
+        dp=list(range(n+1))
+        for i in range(m):
+            lu=dp[0]
+            dp[0]=i+1
+            for j in range(n):
+                dp[j+1],lu=min(dp[j]+1,dp[j+1]+1,lu+int(s1[i]!=s2[j])),dp[j+1]
+        return dp[-1]
